ead1b83474f5a65680a160d7a435f00372bda4fe
Lucas Bortolini <saleslucas99@gmail.com>
2022-07-18 17:17:53 -0300

    start header and rename files to jsx

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="section-header flex justify-center text-white font-bold my-3 py-3">
+      <div className="mx-2 px-2">
+        <a href="#">
+          Blog
+        </a>
+      </div>
+      <div className="mx-2 px-2">
+        <a href="#">
+          Fale Conosco
+        </a>
+      </div>
+      <div className="mx-2 px-2">
+        <a href="#">
+          Motorista
+        </a>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
