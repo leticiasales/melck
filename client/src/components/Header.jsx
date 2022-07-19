9fa80e17ed96a1b574004be9cb507dd3246e5939
Lucas Bortolini <saleslucas99@gmail.com>
2022-07-19 11:07:52 -0300

    Opacity hover with tailwind

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,17 +3,17 @@ import './Header.css';
 function Header() {
   return (
     <header className="section-header flex justify-center text-white font-bold my-3 py-3">
-      <div className="mx-2 px-2">
+      <div className="mx-2 px-2 hover:opacity-80">
         <a href="#">
           Blog
         </a>
       </div>
-      <div className="mx-2 px-2">
+      <div className="mx-2 px-2 hover:opacity-80">
         <a href="#">
           Fale Conosco
         </a>
       </div>
-      <div className="mx-2 px-2">
+      <div className="mx-2 px-2 hover:opacity-80">
         <a href="#">
           Motorista
         </a>
