996e201531f1eb706de74a5e3f0253ff2595090e
Lucas Bortolini <saleslucas99@gmail.com>
2022-07-24 18:13:32 -0300

    configuring container in body

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="container">
+    <div className="container mx-auto">
       <Header />
       <FormTruckloadSearch />
     </div>
