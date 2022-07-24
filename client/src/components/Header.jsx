4c29e5acc62ddd30814e0f7a59900a8b80a865bd
Lucas Bortolini <saleslucas99@gmail.com>
2022-07-24 18:14:31 -0300

    redoing the header with logo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,22 +1,29 @@
 import './Header.css';
 
+import HeaderLogo from "../assets/img/header-logo.png"
+
 function Header() {
   return (
-    <header className="section-header flex justify-center text-white font-bold my-3 py-3">
-      <div className="mx-2 px-2 hover:opacity-80">
-        <a href="#">
-          Blog
-        </a>
+    <header className="container mx-auto justify-center relative w-screen flex mb-8 pt-0 pb-8 px-48">
+      <div className="absolute logo-position">
+        <img src={HeaderLogo} className="w-40"/>
       </div>
-      <div className="mx-2 px-2 hover:opacity-80">
-        <a href="#">
-          Fale Conosco
-        </a>
-      </div>
-      <div className="mx-2 px-2 hover:opacity-80">
-        <a href="#">
-          Motorista
-        </a>
+      <div className="flex justify-center py-8 text-white font-bold">
+        <div className="mx-2 px-2 hover:opacity-80">
+          <a href="/">
+            Blog
+          </a>
+        </div>
+        <div className="mx-2 px-2 hover:opacity-80">
+          <a href="/">
+            Fale Conosco
+          </a>
+        </div>
+        <div className="mx-2 px-2 hover:opacity-80">
+          <a href="/">
+            Motorista
+          </a>
+        </div>
       </div>
     </header>
   );
