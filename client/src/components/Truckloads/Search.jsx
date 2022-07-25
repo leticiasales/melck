8ceac8350a52c6ded804b7131c71b5a9cc815da9
Lucas Bortolini <saleslucas99@gmail.com>
2022-07-25 14:33:30 -0300

    use autocomplete from MUI and responsive sizes

--- client/src/components/Truckloads/Search.jsx
+++ client/src/components/Truckloads/Search.jsx
@@ -30,19 +30,19 @@ class TruckloadsSearch extends Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="w-full max-w-5xl mx-auto pt-8 mt-8 pb-4 mb-4 text-center text-white">
+      <div className="container py-8 my-8 md:py-16 md:my-16">
+        <div className="w-full max-w-5xl mx-auto pb-4 mb-4 text-center text-white">
           <h1 className="block py-2 my-2 text-3xl font-black uppercase">O jeito mais fácil de conseguir sua carga</h1>
           <h2 className="block font-semibold text-lg py-2 my-2">Encontre cargas de acordo com o seu caminhão, origem ou destino</h2>
         </div>
         <form className="w-full max-w-5xl mx-auto">
-          <div className="flex flex-wrap gap-y-4 w-3/4 md:w-full mx-auto mb-2 px-3">
+          <div className="flex md:flex-nowrap flex-wrap gap-y-1 md:gap-y-4 md:gap-x-2 w-full mx-auto mb-4 px-3">
             <Autocomplete
               id="origin"
+              name="origin"
               autoComplete
-              className="appearance-none block w-full md:w-1/2 mb-3 leading-tight"
-              options={this.state.cities}
-              sx={{ padding: 0 }}
+              className="appearance-none w-full block mb-3 leading-tight"
+              options={ this.state.cities }
               renderInput={(params) =>
                 <TextField {...params}
                   className="bg-white rounded-md py-0"
@@ -50,22 +50,35 @@ class TruckloadsSearch extends Component {
                   size="small"
                 />}
               />
-            <div className="w-full md:w-1/2 px-3">
-              <input className="appearance-none block w-full bg-white text-black border border-white rounded-md py-2 px-4 leading-tight placeholder-black focus:outline-none focus:bg-white focus:border-primary" id="grid-last-name" type="text" placeholder="Destino"></input>
-            </div>
+            <Autocomplete
+              id="destination"
+              name="destination"
+              autoComplete
+              className="appearance-none w-full block text-black leading-tight"
+              options={ this.state.cities }
+              renderInput={(params) =>
+                <TextField {...params}
+                className="bg-white rounded-md py-0"
+                placeholder="Destino"
+                size="small"
+                />}
+              />
           </div>
           <div className="flex flex-wrap w-full mb-2 px-3">
             <div className="relative w-full">
-              <select required className="appearance-none block px-3 w-full bg-white mb-0 text-black border border-white rounded-md py-2 px-4 mb-3 leading-tight font-medium focus:outline-none focus:bg-white focus:border-primary" id="grid-state" defaultValue="" >
-                <option value="">Escolha a categoria do seu caminhão</option>
-                {
-                  this.state.vehicles.map((vehicle, i) => 
-                    <option value={vehicle} key={i}>{vehicle}</option>)
-                }
-              </select>
-              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-              </div>
+              <Autocomplete
+                id="vehicle"
+                name="vehicle"
+                required
+                className="appearance-none w-full block text-black leading-tight"
+                options={ this.state.vehicles }
+                renderInput={(params) =>
+                  <TextField {...params}
+                    className="bg-white rounded-md py-0"
+                    placeholder="Escolha a categoria do seu caminhão"
+                    size="small"
+                  />}
+                />
             </div>
           </div>
           <div className="px-3 my-8">
