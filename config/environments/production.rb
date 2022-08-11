707f7a6b3d9ecc1cbd832e0f86294eb241749298
Lucas Bortolini <bortolinilucas20@gmail.com>
2022-08-11 20:32:31 -0300

    fix deploy 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,6 @@ Rails.application.configure do
   config.i18n.enforce_available_locales = false
   config.i18n.available_locales = ["pt-BR"]
   config.i18n.default_locale = :'pt-BR'
+
+  config.assets.compile = true
 end
