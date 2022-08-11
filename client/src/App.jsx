958a32eea2bb8e825905e8989abd6c052e1cb4f6
Lucas Bortolini <saleslucas99@gmail.com>
2022-08-11 20:07:27 -0300

    remove unused css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
 
 import Header from "./components/Header";
 import TruckloadsSearch from "./components/Truckloads/Search";
