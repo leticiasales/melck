5d374183ce81668c030ce4da55385fa960bdec04
Lucas Bortolini <saleslucas99@gmail.com>
2022-08-11 20:14:02 -0300

    enable frontend search and pagination

--- client/src/components/Truckloads/List.jsx
+++ client/src/components/Truckloads/List.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-import { Link } from "react-router-dom";
-import { Autocomplete, TextField } from "@mui/material";
+import {
+  Autocomplete,
+  CircularProgress,
+  TextField,
+  Pagination,
+  PaginationItem
+} from "@mui/material";
+import {
+  KeyboardDoubleArrowLeft,
+  KeyboardDoubleArrowRight
+} from '@mui/icons-material';
+
 import Item from "./Item";
 import Modal from "../Modal";
+
 import "./List.css";
-import left from "../../assets/img/left.svg"
-import right from "../../assets/img/right.svg"
 
 import truckloadListBg from "../../assets/img/truckload-list-bg.png"
 
@@ -15,18 +24,46 @@ class TruckloadsList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      categories: ["Van", "Toco", "Truck", "Bitruck", "Carreta", "Carreta Ls", "Vanderleia", "Bitrem"],
       cities: [],
+      loading: true,
       modalTruckload: false,
+      page: null,
+      totalPages: null,
       truckloads: [],
     };
   }
 
+  updateParams = (e, value) => {
+    e.preventDefault();
+
+    const page = value || this.state.page;
+
+    this.setState({
+      loading: true,
+      page,
+    }, () => { this.search() })
+  }
+
   search() {
+    console.log("state:", this.state);
+    let { origin, destination, vehicle, page } = this.state;
+
+    const params = {
+      origin,
+      destination,
+      vehicle,
+      page
+    };
+
     axios
-      .get("/api/truckloads")
+      .get("/api/truckloads", { params })
       .then((res) => {
-        this.setState({ truckloads: res.data });
+        this.setState({
+          truckloads: res.data.truckloads,
+          loading: false,
+          page: res.data.page,
+          totalPages: res.data.totalPages
+        });
       })
       .catch((error) => console.log(error));
   }
@@ -43,7 +80,14 @@ class TruckloadsList extends Component {
   }
 
   componentDidMount() {
-    this.search();
+    const params = new URLSearchParams(window.location.search);
+    console.log(params);
+    this.setState({
+      origin: params.get("origin"),
+      destination: params.get("destination"),
+      vehicle: params.get("vehicle"),
+    }, () => { this.search() });
+
     this.getCitiesList();
   }
 
@@ -60,20 +104,27 @@ class TruckloadsList extends Component {
   }
 
   render() {
+    const vehicleOptions = ["Van", "Toco", "Truck", "Bitruck", "Carreta", "Carreta Ls", "Vanderleia", "Bitrem"];
+    const { cities, loading, modalTruckload, page, totalPages, truckloads} = this.state;
+
     return (
-      <div>
-        <div className="flex justify-center">
-          <img src={ truckloadListBg } alt="avatar cargas" className="w-5/6"></img>
+      <>
+        <div id="banner" className="pt-3 flex justify-center">
+          <img src={ truckloadListBg } alt="Banner" className="-mt-20 px-4 max-w-5xl"></img>
         </div>
         <div className="bg-secondary py-12">
           <form className="max-w-5xl mx-auto">
             <div className="grid md:grid-cols-4 gap-4 w-full px-4 md:px-24">
               <Autocomplete
-                id="origin-b"
-                name="origin-b"
+                id="origin"
+                name="origin"
                 required
                 className="appearance-none block text-black leading-tight"
-                options={this.state.cities}
+                options={ cities }
+                value={ this.state.origin || null }
+                onChange={(event, value) => 
+                  this.setState({ origin: value })
+                }
                 renderInput={(params) =>
                   <TextField {...params}
                     className="bg-white rounded-md py-0"
@@ -82,10 +133,12 @@ class TruckloadsList extends Component {
                   />}
                 />
               <Autocomplete
-                id="destination-b"
-                name="destination-b"
+                id="destination"
+                name="destination"
                 className="appearance-none block text-black leading-tight"
-                options={this.state.cities}
+                options={ cities }
+                value={ this.state.destination || null }
+                onChange={(event, value) => this.setState({ destination: value })}
                 renderInput={(params) =>
                   <TextField {...params}
                     className="bg-white rounded-md py-0"
@@ -94,11 +147,13 @@ class TruckloadsList extends Component {
                   />}
                 />
               <Autocomplete
-                id="categories-b"
-                name="categories-b"
+                id="vehicle"
+                name="vehicle"
                 required
                 className="appearance-none block text-black leading-tight"
-                options={this.state.categories}
+                options={ vehicleOptions }
+                value={ this.state.vehicle || null }
+                onChange={(event, value) => this.setState({ vehicle: value })}
                 renderInput={(params) =>
                   <TextField {...params}
                     className="bg-white rounded-md py-0"
@@ -106,32 +161,49 @@ class TruckloadsList extends Component {
                     size="small"
                   />}
                 />
-              <Link to="category" className="block bg-tertiary md:mx-1 py-2 px-4 rounded-md hover:opacity-90 text-center text-white uppercase font-bold">
+              <button onClick={ this.updateParams } className="block bg-tertiary md:mx-1 py-2 px-4 rounded-md hover:opacity-90 text-center text-white uppercase font-bold">
                 Buscar
-              </Link>
+              </button>
             </div>
          </form>
         </div>
-        <div className="bg-white">
-          {
-            this.state.truckloads.map((truckload) => {
-              return <Item truckload={ truckload } openModal={ this.openModal }key={ truckload.id } />
-            })
+        <div className="bg-white pt-2 grow flex flex-col justify-center items-center">
+          { loading ?
+            <CircularProgress /> :
+            <>
+              <ul className="w-full bg-white list-style-none">
+                {
+                  truckloads.map((truckload) => {
+                    return <li className="text-center" key={ truckload.id }>
+                      <Item truckload={ truckload } openModal={ this.openModal } />
+                    </li>
+                  })
+                }
+              </ul>          
+              <div className="flex justify-center pt-2 pb-4">
+                <Pagination
+                  count={ totalPages }
+                  page={ page }
+                  siblingCount={1}
+                  boundaryCount={0}
+                  shape="rounded"
+                  onChange={ this.updateParams }
+                  renderItem={(item) =>
+                    <PaginationItem
+                      components={{
+                        previous: KeyboardDoubleArrowLeft,
+                        next: KeyboardDoubleArrowRight
+                      }}
+                      {...item}
+                    />
+                  }
+                />
+              </div>
+            </>
           }
+          <Modal truckload={ modalTruckload } closeModal={ this.closeModal } />
         </div>
-        <div className="bg-white flex justify-center pt-2 pb-4">
-          <button>
-            <img src={ left } alt="página anterior" className="w-5"></img>
-          </button>
-          <span className="text-xl text-gray-500 px-1"> 
-            x
-          </span>
-          <button>
-            <img src={ right } alt="próxima página" className="w-5"></img>
-          </button>
-        </div>
-        <Modal truckload={ this.state.modalTruckload } closeModal={ this.closeModal } />
-      </div>
+      </>
     );
   }
 };
--- client/src/components/Truckloads/Search.jsx
+++ client/src/components/Truckloads/Search.jsx
@@ -1,5 +1,4 @@
 import axios from "axios";
-
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Autocomplete, TextField } from "@mui/material";
@@ -9,7 +8,9 @@ class TruckloadsSearch extends Component {
     super(props);
     this.state = {
       cities: [],
-      vehicles: ["Van", "Toco", "Truck", "Bitruck", "Carreta", "Carreta Ls", "Vanderleia", "Bitrem", "3/4"]
+      origin: "",
+      destination: "",
+      vehicle: "",
     };
   }
 
@@ -24,11 +25,36 @@ class TruckloadsSearch extends Component {
       .catch((error) => console.log(error));
   }
 
+  search = (e) => {
+    e.preventDefault();
+
+    const params = { };
+
+    this.props.router.push({
+      pathname: '/cargas',
+      params
+    })
+  }
+
+  urlWithParams() {
+    const { origin, destination, vehicle } = this.state;
+
+    let url = "/cargas?"
+    if (origin) { url = url + `origin=${ origin }&` }
+    if (destination) { url = url + `&destination=${ destination }&` }
+    if (vehicle) { url = url + `vehicle=${ vehicle }` }
+
+    return encodeURI(url);
+  }
+
   componentDidMount() {
     this.getCitiesList();
   }
 
   render() {
+    const vehicleOptions = ["Van", "Toco", "Truck", "Bitruck", "Carreta", "Carreta Ls", "Vanderleia", "Bitrem", "3/4"];
+    const { cities } = this.state;
+
     return (
       <div className="container py-8 my-8 md:py-16 md:my-16">
         <div className="w-full max-w-5xl mx-auto pb-4 mb-4 text-center text-white">
@@ -43,7 +69,8 @@ class TruckloadsSearch extends Component {
               required
               autoComplete
               className="appearance-none w-full block mb-3 leading-tight"
-              options={ this.state.cities }
+              options={ cities }
+              onChange={(event, value) => this.setState({ origin: value })}
               renderInput={(params) =>
                 <TextField {...params}
                   className="bg-white rounded-md py-0"
@@ -56,12 +83,13 @@ class TruckloadsSearch extends Component {
               name="destination"
               autoComplete
               className="appearance-none w-full block text-black leading-tight"
-              options={ this.state.cities }
+              options={ cities }
+              onChange={(event, value) => this.setState({ destination: value })}
               renderInput={(params) =>
                 <TextField {...params}
-                className="bg-white rounded-md py-0"
-                placeholder="Destino"
-                size="small"
+                  className="bg-white rounded-md py-0"
+                  placeholder="Destino"
+                  size="small"
                 />}
               />
           </div>
@@ -72,7 +100,8 @@ class TruckloadsSearch extends Component {
                 name="vehicle"
                 required
                 className="appearance-none w-full block text-black leading-tight"
-                options={ this.state.vehicles }
+                options={ vehicleOptions }
+                onChange={(event, value) => this.setState({ vehicle: value })}
                 renderInput={(params) =>
                   <TextField {...params}
                     className="bg-white rounded-md py-0"
@@ -83,7 +112,7 @@ class TruckloadsSearch extends Component {
             </div>
           </div>
           <div className="px-3 my-8">
-            <Link to="category" className="min-w-max block bg-primary py-2 px-4 rounded-md hover:opacity-90 text-center text-white uppercase font-bold">
+            <Link to={ this.urlWithParams() } className="min-w-max block bg-primary py-2 px-4 rounded-md hover:opacity-90 text-center text-white uppercase font-bold">
               Procurar cargas
             </Link>
           </div>
