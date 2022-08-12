7ebb8edbe8898f0f93f74b57cd85c3c93f05f526
Lucas Bortolini <saleslucas99@gmail.com>
2022-08-11 23:28:58 -0300

    remove modal css and add masks

--- client/src/components/Truckloads/Modal.jsx
+++ client/src/components/Truckloads/Modal.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { Close, WhatsApp } from '@mui/icons-material';
-import "./Modal.css";
+import NumberFormat from "react-number-format";
+import { Close, WhatsApp } from "@mui/icons-material";
 
 import furgao from "../../assets/img/furgao.png"
 import bau from "../../assets/img/bau.png"
@@ -22,14 +22,16 @@ const iconsMap = {
 class Modal extends Component {
   render() {
     const { closeModal, truckload } = this.props;
-    const showHideClassName = truckload ? "modal block" : "modal hidden";
+    const showHideClassName = truckload ? "fixed" : "hidden";
+    const chargingTime = truckload && new Date(truckload.charging_time).toLocaleTimeString('pt-BR', { hour: '2-digit', minute:'2-digit' });
+    const deliveryDate = truckload && new Date(truckload.delivery_date).toLocaleDateString('pt-BR');
 
     const phone = process.env.REACT_APP_WPP_PHONE;
     const message = encodeURI(process.env.REACT_APP_WPP_MESSAGE);
 
     return (
-    <div className={ showHideClassName }>
-      <section className="modal-main w-100 h-auto bg-white sm:w-75 min-w-full sm:min-w-max rounded-3xl px-4 py-4 -translate-x-2/4 -translate-y-1/2">
+    <div className={ showHideClassName + " bg-black-transparent top-0 left-0 w-screen h-screen" }>
+      <section className="fixed w-4/5 h-auto top-1/2 left-1/2 bg-white sm:w-75 min-w-full sm:min-w-max rounded-3xl px-4 py-4 -translate-x-2/4 -translate-y-1/2">
         <div className="flex flex-col border-2 rounded-3xl border-tertiary px-3 py-2">
           <div className="absolute right-2 top-2">
             <button onClick={ closeModal } type="button" className="relative flex justify-center items-center text-white p-1 rounded-2xl bg-tertiary top-0 right-0 hover:opacity-90">
@@ -48,7 +50,7 @@ class Modal extends Component {
                 { truckload?.material }
               </span>
               <span className="text-lg font-semibold">
-                { truckload?.total_weight }
+                <NumberFormat value={ truckload?.total_weight } thousandSeparator="." decimalSeparator="," suffix=" kg" isNumericString={ true } decimalScale={ 3 } />
               </span>
               <span className="text-lg font-semibold">
                 { truckload?.origin }
@@ -60,20 +62,12 @@ class Modal extends Component {
           </div>
           <div className="mx-0 sm:mx-10 mt-6 mb-4">
             <div className="mb-4">
-              <div className="text-sm font-semibold">
-                <span className="font-bold mr-2">
-                  Data de Embarque: 
-                </span>
-                <span className="font-medium">
-                  { truckload?.delivery_date }
-                </span>
-              </div>
-              <div className="text-sm font-semibold">
+              <div className="text-sm">
                 <span className="font-bold mr-2">
                   Hora limite para carregamento: 
                 </span>
                 <span className="font-medium">
-                  { truckload?.charging_time }
+                  { chargingTime }
                 </span>
               </div>
               <div className="text-sm font-semibold">
@@ -81,7 +75,7 @@ class Modal extends Component {
                   Data de entrega: 
                 </span>
                 <span className="font-medium">
-                  { truckload?.delivery_date }
+                  { deliveryDate }
                 </span>
               </div>
             </div>
@@ -127,7 +121,7 @@ class Modal extends Component {
                   Peso total: 
                 </span>
                 <span className="font-medium">
-                  { truckload?.total_weight }
+                  <NumberFormat value={ truckload?.total_weight } thousandSeparator="." decimalSeparator="," suffix=" kg" isNumericString={ true } decimalScale={ 3 } />
                 </span>
               </div>
             </div>
@@ -137,7 +131,7 @@ class Modal extends Component {
                   Valor da mercadoria: 
                 </span>
                 <span className="font-medium">
-                  { truckload?.price }
+                  <NumberFormat value={ truckload?.price } thousandSeparator="." decimalSeparator="," prefix="R$" isNumericString={ true } decimalScale={ 2 } />
                 </span>
               </div>
             </div>
